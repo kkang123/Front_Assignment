6c39bb9ebcc07d4f7a7e652fc0aa14bee9c508f5
kkang123 <one98kr@gmail.com>
2024-07-24 17:25:03 +0900

    ✨  jsx확장자 사용을 위한 bable 패키치 추가

--- webpack.config.js
+++ webpack.config.js
@@ -17,17 +17,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
+
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new CleanWebpackPlugin(),
